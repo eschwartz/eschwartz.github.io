aa532cb62a4f1beb7dba8c5f28e61c22ea14254e
Edan Schwartz <eschwartz@weathernation.net>
2014-09-04 09:41:09 -0500

    init eschwartz compass project

---
+++ assets/themes/eschwartz/config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+
+sass_options = {:sourcemap => true,}
+enable_sourcemaps = true
